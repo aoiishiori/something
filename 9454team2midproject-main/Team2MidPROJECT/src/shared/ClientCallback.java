cf2587a5ff42eb01e63efc1fec0c960aa8ee61b4
shiori <umuwastaken@gmail.com>
2026-04-06 10:28:20 +0800

    Delete 9454team2midproject-main directory

--- 9454team2midproject-main/Team2MidPROJECT/src/shared/ClientCallback.java
+++
@@ -1,9 +0,0 @@
-package shared;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ClientCallback extends Remote {
-    void onProductUpdated(String productId) throws RemoteException;
-    void onServerMessage(String message) throws RemoteException;
-}
